c9a573ca338ba4b7d97df27dd26c1a858d988338
many0216 <m_ndeepk@live.concordia.ca>
2023-05-19 14:59:53 -0400

    adding classes for package management: Epic_1

---
+++ Hotel.java
@@ -0,0 +1,59 @@
+package org.COEN.models;
+
+// Hotel class representing a hotel component
+public class Hotel {
+    private String hotelId;
+    private String name;
+    private String location;
+    private double price;
+
+    public Hotel(String hotelId, String name, String location, double price) {
+        this.hotelId = hotelId;
+        this.name = name;
+        this.location = location;
+        this.price = price;
+    }
+
+    public String getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(String hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "hotelId='" + hotelId + '\'' +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
+
