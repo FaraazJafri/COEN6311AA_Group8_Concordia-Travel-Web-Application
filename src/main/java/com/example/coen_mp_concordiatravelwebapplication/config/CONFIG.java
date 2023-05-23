e0a89b7b77b50c43a4aef5b74a37290d6d8e29e8
shiveshc710 <shiveshc710@gmail.com>
2023-05-22 23:27:15 -0400

    Config file

---
+++ src/main/java/com/example/coen_mp_concordiatravelwebapplication/config/CONFIG.java
@@ -0,0 +1,10 @@
+package com.example.coen_mp_concordiatravelwebapplication.config;
+
+public class CONFIG {
+
+    public static final String SQLURL = "jdbc:mysql://localhost:3306/travelsystem";
+
+    public static final String SQLUSER = "root";
+
+    public static final String SQLPASS = "Zeba_b38";
+}
